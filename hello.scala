ac624ff47bc2e1b3c1769e3133791e2c902112b4
Jiří Beneš <mail@jiribenes.com>
2022-10-26 15:44:10 +0200

    Add snark

--- hello.scala
+++ hello.scala
@@ -10,13 +10,28 @@ def sayhello = {
     s"""Hello there, ${name}.
       |
       |Seems like you have all dependencies set up correctly, great.
+  println(greeting)
+
+  if (name != osUserName) {
+    val snarkyGreeting =
+      s"""Dear ${name} -- or should I say 'Dear ${osUserName}'?!
+         |
+         |Are you lying to me, or are you lying to your operating system?
+         |""".stripMargin
+    println(snarkyGreeting)
+  }
+
+  val systemInfo =
+    s"""Seems like you have all dependencies set up correctly.
       |Apparently, this application is running on ${os} with JDK ${jdkVersion}.
       |""".stripMargin
 
-  println(greeting)
+  println(systemInfo)
 }
 
 
 def jdkVersion = System.getProperty("java.version")
 
 def os = System.getProperty("os.name")
+
+def osUserName = System.getProperty("user.name")
