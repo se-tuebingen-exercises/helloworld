890326e9aa61c71f58c98e1319277924654bc29c
Jiří Beneš <mail@jiribenes.com>
2022-10-26 15:10:18 +0200

    Be even more excited

--- hello.scala
+++ hello.scala
@@ -6,11 +6,16 @@ def sayHello = {
 
   val name = readLine()
 
+  // Don't forget to buy milk on the way home from the lecture -- there's no more food at home.
+  // TODO: Don't forget to remove the comment above before committing.
+
   val greeting =
-    s"""Hello there, ${name}!
+    s"""Hello there, ${name}, nice to see you!
       |
       |Seems like you have all dependencies set up correctly.
       |Apparently, this application is running on ${os} with JDK ${jdkVersion}.
+      |
+      |Have fun in the Software Engineering course! :)
       |""".stripMargin
 
   println(greeting)
